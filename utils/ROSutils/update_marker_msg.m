dc58a06ab4957537673b8acc5a8e668d1120854d
thomasliu <xl2623@nyu.edu>
2022-01-25 11:03:16 -0500

    tested on ubuntu 20.04, ROS noetic, MATLAB 2022a prerelease

--- utils/ROSutils/update_marker_msg.m
+++ utils/ROSutils/update_marker_msg.m
@@ -1,20 +1,20 @@
 function marker_stamped_msg = update_marker_msg(marker_stamped_msg,pos,attitude,id)
 
-marker_stamped_msg.pose.position.x = pos(1);
-marker_stamped_msg.pose.position.y = pos(2);
-marker_stamped_msg.pose.position.z = pos(3);
-marker_stamped_msg.pose.orientation.w = attitude(1);
-marker_stamped_msg.pose.orientation.x = attitude(2);
-marker_stamped_msg.pose.orientation.y = attitude(3);
-marker_stamped_msg.pose.orientation.z = attitude(4);
+marker_stamped_msg.Pose.Position.X = pos(1);
+marker_stamped_msg.Pose.Position.Y = pos(2);
+marker_stamped_msg.Pose.Position.Z = pos(3);
+marker_stamped_msg.Pose.Orientation.W = attitude(1);
+marker_stamped_msg.Pose.Orientation.X = attitude(2);
+marker_stamped_msg.Pose.Orientation.Y = attitude(3);
+marker_stamped_msg.Pose.Orientation.Z = attitude(4);
 time = rostime('now');
-marker_stamped_msg.header.stamp.sec = uint32(time.Sec);
-marker_stamped_msg.header.stamp.nsec = uint32(time.Nsec);
+marker_stamped_msg.Header.Stamp.Sec = uint32(time.Sec);
+marker_stamped_msg.Header.Stamp.Nsec = uint32(time.Nsec);
 
 if nargin == 3
-    marker_stamped_msg.id = int32(0);
+    marker_stamped_msg.Id = int32(0);
 elseif nargin == 4
-    marker_stamped_msg.id = int32(id);
+    marker_stamped_msg.Id = int32(id);
 else
     error("The input number is wrong \n")
 end
