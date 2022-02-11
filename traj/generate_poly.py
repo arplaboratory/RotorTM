29516360ec75eb1dad4936c84797db152f4b2e1b
xl2623 <90472635+xl2623@users.noreply.github.com>
2022-02-11 14:22:58 -0500

    Add files via upload

---
+++ traj/generate_poly.py
@@ -0,0 +1,13 @@
+from math import factorial
+import numpy as np
+def generate_poly(max_exponent,max_diff,symbol):
+	
+	f=np.zeros((max_diff+1, max_exponent+1), dtype=float)
+	
+	for k in range(max_diff+1):
+		for i in range(max_exponent+1):
+			if (i - k) >= 0:
+				f[k,i]  = factorial(i)*symbol**(i-k)/factorial(i-k)
+			else:
+				f[k,i] = 0
+	return f
