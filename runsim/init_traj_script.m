c223ff9191f6308fb423bfd1149aca9536ab20b0
lguanrui <gl1871@nyu.edu>
2022-04-21 06:43:30 -0400

    add offline traj init in the init_traj_script

--- runsim/init_traj_script.m
+++ runsim/init_traj_script.m
@@ -1,20 +1,20 @@
 %% Initialize circle trajectory
-% radius = 1.0;
-% period = 6.0;
-% duration = 6.0;
-% [~,payload_stop] = circle([], [], payload_start{1}(1:3),radius,period,duration);
-% trajhandle = @circle;
+radius = 1.0;
+period = 6.0;
+duration = 6.0;
+[~,payload_stop] = circle([], [], payload_start{1}(1:3),radius,period,duration);
+trajhandle = @circle;
 
 %% Initialize line trajectory
-line_quintic_traj_generator([], [],map, payload_path);
-trajhandle = @line_quintic_traj_generator;
+% line_quintic_traj_generator([], [],map, payload_path);
+% trajhandle = @line_quintic_traj_generator;
 
 %% Initialize Minimum kth derivative trajectory
 % [pathlength, ~] = size(payload_path);
 % traj_constant.max_diff = 6;
 % traj_constant.max_exponent = 11;
-% traj_constant.max_vel = 3;
-% traj_constant.max_acc = 10;
+% traj_constant.max_vel = 2.0;
+% traj_constant.max_acc = 4.0;
 % traj_constant.dim = 3;
 % traj_constant.num_coeff = traj_constant.max_exponent +1;
 % traj_constant.total_traj_num = pathlength - 1;
@@ -23,6 +23,10 @@ trajhandle = @line_quintic_traj_generator;
 % traj_constant.pt_num = pathlength;
 % traj_constant.cor_constraint = false;
 % traj_constant.max_iteration = 300;
-% min_snap_traj_generator([], [], map, payload_path, traj_constant);
+% min_snap_traj_generator([], [], payload_path, traj_constant);
 % trajhandle = @min_snap_traj_generator;
+
+%% Offline generated traj
+% offline_traj([],[],"rigid_links_traj.mat")
+% trajhandle = @offline_traj;
 
